dc67231859cce2f133f75a372dc2fcceae723672
Michael Greenberg <michael.greenberg@stevens.edu>
2023-11-07 16:59:59 -0500

    accessing sys.argv

--- test.py
+++ test.py
@@ -1,2 +1,6 @@
+import sys
+
 if __name__ == "__main__":
-    print('wow i really need to implement this')
+    print(f'got {len(sys.argv)} arguments:')
+    for arg in sys.argv:
+        print(arg)
