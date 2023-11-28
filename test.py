ac1de8bd6fab95f91801f44c9c10ca1106d22149
Michael Greenberg <michael.greenberg@stevens.edu>
2023-11-28 16:02:14 -0500

    enum args (keeping spaces)

--- test.py
+++ test.py
@@ -5,5 +5,7 @@ import sys
 if __name__ == "__main__":
     print(f'got {len(sys.argv)} arguments:')
 
+    i = 0
     for arg in sys.argv:
-        print(f'    {arg}')
+        print(f'    {i}: {arg}')
+        i += 1
