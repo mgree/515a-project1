d28748b87881d4690b8268d35f1bdad0bbebb5ae
Michael Greenberg <michael.greenberg@stevens.edu>
2023-11-28 16:00:58 -0500

    manually revert to initial state

--- test.py
+++ test.py
@@ -5,7 +5,5 @@ import sys
 if __name__ == "__main__":
     print(f'got {len(sys.argv)} arguments:')
 
-    i = 0
     for arg in sys.argv:
-        print(f'    {i}: {arg}')
-        i += 1
+        print(f'\t{arg}')
